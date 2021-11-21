855a264dac789a9df9c8ec40c22b46339510251e
iokim <iokim1220@gmail.com>
2021-11-21 23:34:34 +0900

    # 관심사의 분리

--- src/test/java/hello/core/discount/ReateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/ReateDiscountPolicyTest.java
@@ -2,7 +2,6 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +10,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class ReateDiscountPolicyTest {
 
-    ReateDiscountPolicy discountPolicy = new ReateDiscountPolicy();
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다")
